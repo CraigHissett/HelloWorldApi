1a12d946b1b85d8c1b08606c3d42ef2ffb5e11bc
Craig Hissett <craig@users.noreply.github.com>
2026-06-05 15:46:13 +0000

    feat: add /weatherforecast endpoint and record type

--- Program.cs
+++ Program.cs
@@ -22,4 +22,24 @@ app.MapGet("/hello", () => Results.Ok("Hello, World!"))
    .WithName("GetHello")
    .WithOpenApi();
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast(
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+    return Results.Ok(forecast);
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
 app.Run();
